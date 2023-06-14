763553ac90560d403049641df31ad5396b9d86d6
simoS1997 <116726671+simoS1997@users.noreply.github.com>
2023-06-14 12:56:04 +0200

    Delete 10.py

--- 10.py
+++
@@ -1,9 +0,0 @@
-def div(a, b):
-    try:
-        r = a / b
-    except ZeroDivisionError:
-        print("Error, el segon paràmetre és zero!")
-    else:
-        print("El resultat de dividir {} i {} és {}".format(a, b, r))
-
-div(3, 0)
